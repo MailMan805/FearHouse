20b722965bd2968aab11261ef5be94f2586fcf60
ShinyVictinis <theshiningvictini@gmail.com>
2025-02-28 19:53:43 -0600

    test rooms

--- Assets/Scripts/GenManager.cs
+++ Assets/Scripts/GenManager.cs
@@ -27,7 +27,7 @@ public class GenManager : MonoBehaviour
     void Start()
     {
         // targets the Overlap LayerMask for overlap detection
-        rooms = Resources.LoadAll<GameObject>("Rooms");
+        rooms = Resources.LoadAll<GameObject>("HaydenRooms");
         GameObject genRoom = Instantiate(rooms[0]) as GameObject;
         genRoom.transform.position = spawn;
         rootParts.Add(genRoom);
