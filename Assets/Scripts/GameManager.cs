2e063df4eb02d25a4dc068823fb3661781ec93fa
WEBSTERAD\WUStudent <dts@webster.edu>
2025-04-11 18:57:31 -0500

    grsgs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,17 +24,6 @@ public class GameManager : MonoBehaviour
 
     void Awake()
     {
-        SpawnPlayers();
     }
 
-    void SpawnPlayers()
-    {
-        // Spawn Player 1
-        GameObject player1 = Instantiate(player1Prefab, spawnPoints[0].position, Quaternion.identity);
-        player1.GetComponent<PlayerController>().playerNumber = 1; // Assign Player 1 number
-
-        // Spawn Player 2
-        GameObject player2 = Instantiate(player2Prefab, spawnPoints[1].position, Quaternion.identity);
-        player2.GetComponent<PlayerController>().playerNumber = 2; // Assign Player 2 number
-    }
 }
